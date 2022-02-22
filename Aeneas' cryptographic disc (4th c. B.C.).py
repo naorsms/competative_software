6299ec9b9cb2864d088f36eeab2a3c7145c1f243
kfiratia <82805029+kfiratia@users.noreply.github.com>
2022-02-22 11:33:37 +0200

    Update Aeneas' cryptographic disc (4th c. B.C.).py

--- Aeneas' cryptographic disc (4th c. B.C.).py
+++ Aeneas' cryptographic disc (4th c. B.C.).py
@@ -1,26 +1,29 @@
 import math
 radius = float(input())
-i = 0
+
 dict = {}
-while i < 26:
-    parse = input()
+letters = []
+find_letter = []
+for i in range(26):
+    parse= input()
     dict[parse.split(" ")[0]] = float(parse.split(" ")[1])
-    i += 1
-sentence = input()
-# for index in sentence:
-#     if not index.isalpha():
-#         sentence = sentence.replace(index,'')
-sentence = sentence.upper()
+    letters.append(parse.split(" ")[0])
+sentence = input().upper()
+for letter in sentence:
+    if letter in letters:
+         find_letter.append(letter)
+arr = []
+arr.append(find_letter[0])
+for i in range(1, len(find_letter)):
+    if find_letter[i] != arr[-1]:
+        arr.append(find_letter[i])
 sum = radius
-for letter in range(1, len(sentence)):
-    if not sentence[letter].isalpha():
-        sentence = sentence.replace(sentence[letter], '')
-    if letter >= len(sentence):
-        break
-    angle = abs(dict[sentence[letter]] - dict[sentence[letter-1]])
+for letter in range(1, len(arr)):
+    angle = abs(dict[arr[letter]] - dict[arr[letter-1]])
     if angle > 180:
         angle = 360 - angle
     if angle != 0:
         sum += math.sqrt(2*math.pow(radius,2) - 2*radius*radius*math.cos(math.radians(angle)))
 
 print(math.ceil(sum))
+
