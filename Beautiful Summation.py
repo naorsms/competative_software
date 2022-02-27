432fa0008fef544f1ab03482e91f1482b59d74fc
naorsms <naorsms96@gmail.com>
2022-02-27 19:42:11 +0200

    14 exrecises down

---
+++ Beautiful Summation.py
@@ -0,0 +1,18 @@
+string_num = int(input())
+dic = dict()
+
+for n in range(string_num):
+    coupon = input().replace('-', '')
+    for i in range(len(coupon)):
+        string = coupon[:i] + "*" + coupon[i + 1:]
+        if string in dic:
+            dic[string] += 1
+        else:
+            dic[string] = 1
+Sum = 0
+for index in dic:
+    appearances = dic[index]
+    if appearances > 1:
+        for i in range(1, appearances):
+            Sum += i
+print(Sum)
