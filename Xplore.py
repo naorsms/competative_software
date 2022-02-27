ad64e759fe88cea8905039d7d7a26154061d1268
kfiratia <82805029+kfiratia@users.noreply.github.com>
2022-02-27 03:15:19 +0200

    Add files via upload

---
+++ Xplore.py
@@ -0,0 +1,31 @@
+import json
+n = int(input())
+dic = {}
+names = []
+for _ in range(n):
+    data = json.loads(input())
+    for author in data['authors']['authors']:
+        name = author['full_name']
+        if name not in dic:
+            dic[name] = [data['citing_paper_count']]
+            names.append(name)
+        else:
+            dic[name].append(data['citing_paper_count'])
+answer = {}
+for name in range(len(dic)):
+    x = sorted(dic[names[name]],reverse=True)
+    leng  = len(x)
+    print(x)
+    answer[names[name]] = ' '
+    for j in range(leng):
+        if x[j] < j +1:
+            print(names[name],"kfirr")
+            answer[names[name]] = j
+            break
+    if answer[names[name]] == ' ':
+        answer[names[name]] = j + 1
+answersort = sorted(answer.items(), key=lambda x:(-x[1],x[0]))
+le = len(answersort)
+for i in range(le):
+    print(answersort[i][0],answersort[i][1])
+
