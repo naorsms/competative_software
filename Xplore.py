7d595c1a6f440edbe6dffcb4c9adb0bd6281a9c2
kfiratia <82805029+kfiratia@users.noreply.github.com>
2022-02-27 10:44:14 +0200

    Update Xplore.py

--- Xplore.py
+++ Xplore.py
@@ -2,20 +2,18 @@ import json
 n = int(input())
 dic = {}
 names = []
-for _ in range(n):
+for index in range(n):
     data = json.loads(input())
     for author in data['authors']['authors']:
-        name = author['full_name']
-        if name not in dic:
-            dic[name] = [data['citing_paper_count']]
-            names.append(name)
+        if author['full_name'] not in names:
+            dic[author['full_name']] = [data['citing_paper_count']]
+            names.append(author['full_name'])
         else:
-            dic[name].append(data['citing_paper_count'])
+            dic[author['full_name']].append(data['citing_paper_count'])
 answer = {}
 for name in range(len(dic)):
     x = sorted(dic[names[name]],reverse=True)
     leng  = len(x)
-    print(x)
     answer[names[name]] = ' '
     for j in range(leng):
         if x[j] < j +1:
@@ -27,4 +25,3 @@ answersort = sorted(answer.items(), key=lambda x:(-x[1],x[0]))
 le = len(answersort)
 for i in range(le):
     print(answersort[i][0],answersort[i][1])
-
