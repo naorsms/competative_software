9a5f029f96167c6ed58376f31d28bd3e2bbb619e
kfiratia <82805029+kfiratia@users.noreply.github.com>
2022-03-03 01:20:18 +0200

    Add files via upload

---
+++ Full Adder.py
@@ -0,0 +1,28 @@
+data = input()
+print1 = input()
+print2 = input()
+print3 = input()
+print4 = input()
+base = int(data.split(" ")[0])
+num = data.split(" ")[1]
+nums = {}
+for i in range(base):
+    nums[num[i]] = i
+nums[" "] = 0
+sum = 0
+sumall = ""
+flag = 0
+for i in range(len(print1)-1,0,-1):
+    sum += nums[print1[i]]+nums[print2[i]] + flag
+    if sum >= base:
+        flag = 1
+        sum = sum%base
+    else:
+        flag = 0
+    sumall += num[sum]
+    sum = 0
+print(data)
+print(print1)
+print(print2)
+print(print3)
+print(" " + ''.join(reversed(sumall)))
