9fbdbe1e04107e7e7510012c9d725b34953f0b80
naorsms <54001211+naorsms@users.noreply.github.com>
2022-03-02 16:07:26 +0200

    Update Kabloom.c

--- Kabloom.c
+++ Kabloom.c
@@ -48,34 +48,40 @@ int values(char x, char y) {
 			return 15;
 		case 'J':
 			return 15;
+		case 'T':
+			return 10;
 		default:
 			return x - '0';
 		}
 	}
 }
+char lcs1[1001][5], lcs2[1001][5];
+int n, arr[1001][1001];
 int main() {
-	char lcs1[1001], lcs2[1001];
-	int n, arr[1001][1001];
+
 	while (scanf("%d", &n) != 0) {
+		if (n == 0)
+			break;
+		//printf("%d\n",n);
 		for (int i = 1; i <= n; i++)
-			scanf("%c", &lcs1[i]);
+			scanf("%s", lcs1[i]);
 		for (int i = 1; i <= n; i++)
-			scanf("%c", &lcs2[i]);
+			scanf("%s", lcs2[i]);
 		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j <= n + 1; j++) {
+			for (int j = 1; j <= n; j++) {
 				if (arr[i][j - 1] > arr[i - 1][j])
 					arr[i][j] = arr[i][j - 1];
 				else
 					arr[i][j] = arr[i - 1][j];
-				if (lcs1[i] == lcs2[j] || lcs1[i] == 'R' || lcs2[j] == 'R') {
-					if (arr[i][j] < arr[i - 1][j - 1] + values(lcs1[i], lcs2[j]))
-						arr[i][j] = arr[i - 1][j - 1] + values(lcs1[i], lcs2[j]);
+				if (lcs1[i][0] == lcs2[j][0] || lcs1[i][0] == 'R' || lcs2[j][0] == 'R') {
+					if (arr[i][j] < arr[i - 1][j - 1] + values(lcs1[i][0], lcs2[j][0]))
+						arr[i][j] = arr[i - 1][j - 1] + values(lcs1[i][0], lcs2[j][0]);
 				}
 			}
 		}
 		printf("%d\n", arr[n][n] * 2);
 		//printf('\n')
-		scanf("%d", &n);
+		//n = scanf("%d", &n);
 	}
 }
 
