206cb7350a8826c3ff8026e5ce9e35587f2f1db6
naorsms <54001211+naorsms@users.noreply.github.com>
2022-03-06 15:30:57 +0200

    Update Mosaic Decoration II.py

--- Mosaic Decoration II.py
+++ Mosaic Decoration II.py
@@ -6,16 +6,16 @@ A = int(data.split(" ")[2])
 B = int(data.split(" ")[3])
 M = int(data.split(" ")[4])
 C = int(data.split(" ")[5])
-width_of_well = W//A
-height_of_well = H//B
+width_of_wall = W//A
+height_of_wall = H//B
 cuts = 0
 if W%A != 0:
     cuts += H
-    width_of_well += 1
+    width_of_wall += 1
 if H%B != 0:
     cuts += W
-    height_of_well +=1
-area = width_of_well*height_of_well
+    height_of_wall +=1
+area = width_of_wall*height_of_wall
 if area%10 == 0:
     sum = area//10
 else:
