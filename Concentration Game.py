25424750020372ec0a7a801e3d14adaa7e558868
kfiratia <82805029+kfiratia@users.noreply.github.com>
2022-03-07 18:14:23 +0200

    Update Concentration Game.py

--- Concentration Game.py
+++ Concentration Game.py
@@ -7,8 +7,6 @@ for i in range(1,2*N,2):
     x = input()
     if x == 'MATCH':
         continue
-    if x == '-1':
-        print (-1)
     for j in range(2):
         card = (int(x.split(" ")[j]))
         if cards[card] == 0:
